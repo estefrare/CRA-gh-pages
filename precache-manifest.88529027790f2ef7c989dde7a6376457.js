9882ea64c295ce9734212b908c4cf829ffbe2a4f
Esteban Frare <esteban.frare@radiumrocket.com>
2019-10-29 20:23:54 -0300

    Updates

---
+++ precache-manifest.88529027790f2ef7c989dde7a6376457.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e513018e2042c8bb0dc408d0c14d09ff",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "e421eefde9617c6d3326",
+    "url": "/repository-name/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6272c9d95068076152ae",
+    "url": "/repository-name/static/js/2.c4c118c1.chunk.js"
+  },
+  {
+    "revision": "e421eefde9617c6d3326",
+    "url": "/repository-name/static/js/main.bcf64461.chunk.js"
+  },
+  {
+    "revision": "d915ee6a57b5e0c3caea",
+    "url": "/repository-name/static/js/runtime-main.72b870ca.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/repository-name/static/media/logo.25bf045c.svg"
+  }
+]);
